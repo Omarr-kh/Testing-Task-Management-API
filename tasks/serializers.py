763f0ae5cf2c7ad18c90cd795c38f9eb93aad8b4
Omar <omarkhaled1752003@yahoo.com>
2024-09-01 16:11:15 +0300

    code formatted

--- tasks/serializers.py
+++ tasks/serializers.py
@@ -6,7 +6,7 @@ class TaskSerializer(serializers.ModelSerializer):
     class Meta:
         model = Task
         fields = ["title", "description", "status"]
-    
+
     def create(self, validated_data):
         validated_data["user"] = self.context["request"].user
         return super().create(validated_data)
