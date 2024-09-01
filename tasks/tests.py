8af2b7d936b02e225c19bd7bcb3a2f5783e599f3
Omar <omarkhaled1752003@yahoo.com>
2024-09-01 16:09:10 +0300

    tests for "List" api view

--- tasks/tests.py
+++ tasks/tests.py
@@ -49,3 +49,34 @@ class TaskAPICreate(APITestCase):
         response = self.client.post(url, task_data, format="json")
         self.assertEqual(response.status_code, 400)
         self.assertEqual(Task.objects.count(), 1)
+
+
+class TaskAPIList(APITestCase):
+    def setUp(self) -> None:
+        self.user = User.objects.create_user(username="omar", password="12345")
+        self.client.login(username="omar", password="12345")
+        self.task = Task.objects.create(
+            user=self.user, title="Title #1 test", description="Description #1 test"
+        )
+
+    def test_list_view(self) -> None:
+        url = reverse("list-create-tasks")
+        response = self.client.get(url, format="json")
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.data), 1)
+
+    def test_list_view_after_create(self) -> None:
+        Task.objects.create(
+            user=self.user, title="Title #2", description="Description #2"
+        )
+        url = reverse("list-create-tasks")
+        response = self.client.get(url, format="json")
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.data), 2)
+
+    def test_list_view_after_delete(self) -> None:
+        Task.objects.last().delete()
+        url = reverse("list-create-tasks")
+        response = self.client.get(url, format="json")
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.data), 0)
